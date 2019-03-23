55edb3e48e5543054e4f75eb62f22fa15990e59c
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-03-23 11:59:00 +0100

    Create class Post with msql field

---
+++ src/main/java/com/romanstetsiuk/model/Post.java
@@ -0,0 +1,76 @@
+package com.romanstetsiuk.model;
+
+import net.bytebuddy.asm.Advice;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @Basic
+    private LocalDateTime createdAt;
+
+
+    public Post(String title, String content, LocalDateTime createdAt) {
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public Post() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
